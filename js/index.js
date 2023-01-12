48fbb936fe76b768fd352e9082b9cad43cf3815f
Negrura <68797490+NicolasNeg@users.noreply.github.com>
2023-01-11 23:00:57 -0600

    First commit
    #fecha: 11/01/2023
    ###Cambios
    >No tiene registro o inicio de sesion funcional *php*
    >Css y todo normal

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/*
+function OcultarDiv(){
+    var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    alert('1')
+  } else {
+    x.style.display = "none";
+    MostrarFrase();
+  }
+}
+function MostrarDiv(){
+    var x = document.getElementById("div");
+    x.style.display = "block";
+}
+*/
